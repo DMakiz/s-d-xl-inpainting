0692aca10c675512aa97399e108009d82ad98bd1
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-21 00:05:00 +0300

    Update appl.py

--- appl.py
+++ appl.py
@@ -1,13 +1,5 @@
 import gradio as gr
-import torch
-from PIL import Image
-from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
-import diffusers
-from share_btn import community_icon_html, loading_icon_html, share_js
 from PIL import Image, ImageOps
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-pipe = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16").to(device)
 
 def read_content(file_path: str) -> str:
     """read the content of target file
@@ -18,28 +10,9 @@ def read_content(file_path: str) -> str:
     return content
 
 
-def predict(dict, invert_mask=False, prompt="", negative_prompt="", guidance_scale=7.5, steps=20, strength=1.0, scheduler="EulerDiscreteScheduler"):
-    if negative_prompt == "":
-        negative_prompt = None
-    scheduler_class_name = scheduler.split("-")[0]
-
-    add_kwargs = {}
-    if len(scheduler.split("-")) > 1:
-        add_kwargs["use_karras"] = True
-    if len(scheduler.split("-")) > 2:
-        add_kwargs["algorithm_type"] = "sde-dpmsolver++"
-
-    scheduler = getattr(diffusers, scheduler_class_name)
-    pipe.scheduler = scheduler.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", subfolder="scheduler", **add_kwargs)
-
-    init_image = dict["image"].convert("RGB").resize((1024, 1024))
-    mask = dict["mask"].convert("RGB").resize((1024, 1024))
-    
-    if invert_mask:
-        mask = ImageOps.invert(mask)
-    
-    output = mask
-    return output, gr.update(visible=True)
+def generate_mask(image):
+    mask = image
+    return mask
 
 
 css = '''
@@ -85,34 +58,13 @@ with image_blocks as demo:
     with gr.Row():
         with gr.Column():
             image = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload", height=400)
-            with gr.Row(elem_id="prompt-container", mobile_collapse=False, equal_height=True):
-                with gr.Row():
-                    prompt = gr.Textbox(placeholder="Your prompt (what you want in place of what is erased)", show_label=False, elem_id="prompt")
-                    btn = gr.Button("Inpaint!", elem_id="run_button")
-            
-            with gr.Row(mobile_collapse=False, equal_height=True):
+            with gr.Row():
                 invert_mask_checkbox = gr.Checkbox(label="Invert Mask", initial_value=False, elem_id="invert_mask_checkbox")
-                guidance_scale = gr.Number(value=7.5, minimum=1.0, maximum=20.0, step=0.1, label="guidance_scale")
-                steps = gr.Number(value=20, minimum=10, maximum=30, step=1, label="steps")
-                strength = gr.Number(value=0.99, minimum=0.01, maximum=0.99, step=0.01, label="strength")
-                negative_prompt = gr.Textbox(label="negative_prompt", placeholder="Your negative prompt", info="what you don't want to see in the image")
-            with gr.Row(mobile_collapse=False, equal_height=True):
-                schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
-                scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
-        
+                generate_button = gr.Button("Generate Mask", elem_id="generate_button")
         with gr.Column():
-            image_out = gr.Image(label="Output", elem_id="output-img", height=400)
-            with gr.Group(elem_id="share-btn-container", visible=False) as share_btn_container:
-                community_icon = gr.HTML(community_icon_html)
-                loading_icon = gr.HTML(loading_icon_html)
-                share_button = gr.Button("Share to community", elem_id="share-btn", visible=True)
-            
-
-    btn.click(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container], api_name='run')
-    invert_mask_checkbox.change(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container])
-    prompt.submit(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container])
-    share_button.click(None, [], [], _js=share_js)
-
+            mask_out = gr.Image(label="Mask Output", elem_id="mask_output", height=400)
+    
+    generate_button.click(generate_mask, inputs=[image], outputs=[mask_out])
 
     gr.HTML(
         """
