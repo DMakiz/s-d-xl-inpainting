9e16fd0416a75480a95baa534795ead998aaa183
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-20 14:20:56 +0300

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,13 +1,14 @@
 import gradio as gr
 import torch
+import numpy as np
 from PIL import Image
 
-from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
+from diffusers import AutoPipelineForInpainting
 import diffusers
 from share_btn import community_icon_html, loading_icon_html, share_js
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-pipe = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16").to(device)
+pipe = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float32, variant="fp32").to(device)
 
 def read_content(file_path: str) -> str:
     """read the content of target file
@@ -34,14 +35,13 @@ def predict(image, mask, prompt="", negative_prompt="", guidance_scale=7.5, step
     mask = Image.fromarray(mask)
     mask = mask.convert("L")
     mask = mask.resize((1024, 1024))
-    mask = np.array(mask)
-    mask = np.invert(mask.astype(bool)).astype(np.uint8) * 255
+    mask_array = np.array(mask)
     
-    output = pipe(prompt=prompt, negative_prompt=negative_prompt, image=image, mask_image=mask, guidance_scale=guidance_scale, num_inference_steps=int(steps), strength=strength)
+    output = pipe(prompt=prompt, negative_prompt=negative_prompt, image=image, mask_image=mask_array, guidance_scale=guidance_scale, num_inference_steps=int(steps), strength=strength)
     
-    inverted_mask = np.invert(mask)
+    output_image = Image.fromarray(output.images[0])
     
-    return inverted_mask, gr.update(visible=True)
+    return output_image, gr.update(visible=True)
 
 
 css = '''
@@ -87,7 +87,7 @@ with image_blocks as demo:
     with gr.Row():
                 with gr.Column():
                     image = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload",height=400)
-                    mask = gr.Image(source='upload', tool='sketch', elem_id="mask_upload", type="numpy", label="Mask",height=400)
+                    mask = gr.Image(source='upload', tool='sketch', elem_id="mask_upload", type="pil", label="Mask",height=400)
                     with gr.Row(elem_id="prompt-container", mobile_collapse=False, equal_height=True):
                         with gr.Row():
                             prompt = gr.Textbox(placeholder="Your prompt (what you want in place of what is erased)", show_label=False, elem_id="prompt")
@@ -102,42 +102,30 @@ with image_blocks as demo:
                         with gr.Row(mobile_collapse=False, equal_height=True):
                             schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
                             scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
-                
-                with gr.Column():
-                    mask_out = gr.Image(label="Inverted Mask", elem_id="mask_output", height=400)
-                    with gr.Group(elem_id="share-btn-container", visible=False) as share_btn_container:
-                        community_icon = gr.HTML(community_icon_html)
-                        loading_icon = gr.HTML(loading_icon_html)
-                        share_button = gr.Button("Share to community", elem_id="share-btn",visible=True)
     
-    btn.click(fn=predict, inputs=[image, mask, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[mask_out, share_btn_container], api_name='run')
-    prompt.submit(fn=predict, inputs=[image, mask, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[mask_out, share_btn_container])
-    share_button.click(None, [], [], _js=share_js)
+    with image_blocks.set_block(label="Output"):
+        output_image = gr.Image(label="Output Image", elem_id="output-img", height=400)
+    
+    btn.click(fn=predict, inputs=[image, mask, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[output_image], api_name='run')
+    prompt.submit(fn=predict, inputs=[image, mask, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[output_image])
     
     gr.Examples(
-                examples=[
-                    ["./imgs/aaa (8).png"],
-                    ["./imgs/download (1).jpeg"],
-                    ["./imgs/0_oE0mLhfhtS_3Nfm2.png"],
-                    ["./imgs/02_HubertyBlog-1-1024x1024.jpg"],
-                    ["./imgs/jdn_jacques_de_nuce-1024x1024.jpg"],
-                    ["./imgs/c4ca473acde04280d44128ad8ee09e8a.jpg"],
-                    ["./imgs/canam-electric-motorcycles-scaled.jpg"],
-                    ["./imgs/e8717ce80b394d1b9a610d04a1decd3a.jpeg"],
-                    ["./imgs/Nature___Mountains_Big_Mountain_018453_31.jpg"],
-                    ["./imgs/Multible-sharing-room_ccexpress-2-1024x1024.jpeg"],
-                ],
-                fn=predict,
-                inputs=[image, mask],
-                cache_examples=False,
+        examples=[
+            ["./imgs/aaa (8).png", "./imgs/mask.png"],
+            ["./imgs/download (1).jpeg", "./imgs/mask.png"],
+            ["./imgs/0_oE0mLhfhtS_3Nfm2.png", "./imgs/mask.png"],
+            ["./imgs/02_HubertyBlog-1-1024x1024.jpg", "./imgs/mask.png"],
+            ["./imgs/jdn_jacques_de_nuce-1024x1024.jpg", "./imgs/mask.png"],
+            ["./imgs/c4ca473acde04280d44128ad8ee09e8a.jpg", "./imgs/mask.png"],
+            ["./imgs/canam-electric-motorcycles-scaled.jpg", "./imgs/mask.png"],
+            ["./imgs/e8717ce80b394d1b9a610d04a1decd3a.jpeg", "./imgs/mask.png"],
+            ["./imgs/Nature___Mountains_Big_Mountain_018453_31.jpg", "./imgs/mask.png"],
+            ["./imgs/Multible-sharing-room_ccexpress-2-1024x1024.jpeg", "./imgs/mask.png"],
+        ],
+        filter_fn=lambda img, msk: np.array(Image.open(msk)).shape == (1024, 1024),
+        fn=predict,
+        inputs=[image, mask],
+        cache_examples=False,
     )
-    gr.HTML(
-        """
-            <div class="footer">
-                <p>Model by <a href="https://huggingface.co/diffusers" style="text-decoration: underline;" target="_blank">Diffusers</a> - Gradio Demo by 🤗 Hugging Face
-                </p>
-            </div>
-        """
-    )
-
+    
 image_blocks.queue(max_size=25).launch(debug=True, max_threads=True, share=True, inbrowser=True)
