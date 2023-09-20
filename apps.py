510b3765d369899ecbec06f5458085534e272c8d
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-20 15:29:48 +0300

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,64 +1,89 @@
-import torch
-from diffusers import AutoPipelineForInpainting
 import gradio as gr
+import torch
+
+from diffusers import AutoPipelineForInpainting, UNet2DConditionModel
+import diffusers
+from share_btn import community_icon_html, loading_icon_html, share_js
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 pipe = AutoPipelineForInpainting.from_pretrained("diffusers/stable-diffusion-xl-1.0-inpainting-0.1", torch_dtype=torch.float16, variant="fp16").to(device)
 
-# Добавьте свои стили здесь
+def read_content(file_path: str) -> str:
+    """read the content of target file
+    """
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    return content
+
+def predict(dict, prompt="", negative_prompt="", guidance_scale=7.5, steps=20, strength=1.0, scheduler="EulerDiscreteScheduler"):
+    if negative_prompt == "":
+        negative_prompt = None
+    scheduler_class_name = scheduler.split("-")[0]
+
+    add_kwargs = {}
+    if len(scheduler.split("-")) > 1:
+        add_kwargs["use_karras"] = True
+    if len(scheduler.split("-")) > 2:
+        add_kwargs["algorithm_type"] = "sde-dpmsolver++"
+
+    scheduler = getattr(diffusers, scheduler_class_name)
+    pipe.scheduler = scheduler.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", subfolder="scheduler", **add_kwargs)
+    
+    init_image = dict["image"].convert("RGB").resize((1024, 1024))
+    mask = dict["mask"].convert("RGB").resize((1024, 1024))
+    
+    output = pipe(prompt=prompt, negative_prompt=negative_prompt, image=init_image, mask_image=mask, guidance_scale=guidance_scale, num_inference_steps=int(steps), strength=strength)
+    
+    return output.images[0], output.masks[0], gr.update(visible=True)
+
+
 css = '''
-.gradio-container{max-width: 1100px !important}
-#image_upload{min-height:400px}
-#image_upload [data-testid="image"], #image_upload [data-testid="image"] > div{min-height: 400px}
-#mask_radio .gr-form{background:transparent; border: none}
-#word_mask{margin-top: .75em !important}
-#word_mask textarea:disabled{opacity: 0.3}
-.footer {margin-bottom: 45px;margin-top: 35px;text-align: center;border-bottom: 1px solid #e5e5e5}
-.footer>p {font-size: .8rem; display: inline-block; padding: 0 10px;transform: translateY(10px);background: white}
-.dark .footer {border-color: #303030}
-.dark .footer>p {background: #0b0f19}
-.acknowledgments h4{margin: 1.25em 0 .25em 0;font-weight: bold;font-size: 115%}
-#image_upload .touch-none{display: flex}
-@keyframes spin {
-    from {
-        transform: rotate(0deg);
-    }
-    to {
-        transform: rotate(360deg);
-    }
-}
-#share-btn-container {padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; max-width: 13rem; margin-left: auto;}
-div#share-btn-container > div {flex-direction: row;background: black;align-items: center}
-#share-btn-container:hover {background-color: #060606}
-#share-btn {all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.5rem !important; padding-bottom: 0.5rem !important;right:0;}
-#share-btn * {all: unset}
-#share-btn-container div:nth-child(-n+2){width: auto !important;min-height: 0px !important;}
-#share-btn-container .wrap {display: none !important}
-#share-btn-container.hidden {display: none!important}
-#prompt input{width: calc(100% - 160px);border-top-right-radius: 0px;border-bottom-right-radius: 0px;}
-#run_button{position:absolute;margin-top: 11px;right: 0;margin-right: 0.8em;border-bottom-left-radius: 0px;
-    border-top-left-radius: 0px;}
-#prompt-container{margin-top:-18px;}
-#prompt-container .form{border-top-left-radius: 0;border-top-right-radius: 0}
-#image_upload{border-bottom-left-radius: 0px;border-bottom-right-radius: 0px}
+
 '''
 
-def predict(mask):
-    mask_image = mask.convert("L").resize((1024, 1024))
-    
-    output = pipe(mask_image=mask_image)
-    
-    return output["output_mask"].cpu().numpy()
+image_blocks = gr.Blocks(css=css, elem_id="total-container")
+with image_blocks as demo:
+    gr.HTML(read_content("header.html"))
+    with gr.Row():
+                with gr.Column():
+                    image = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload",height=400)
+                    with gr.Row(elem_id="prompt-container", mobile_collapse=False, equal_height=True):
+                        with gr.Row():
+                            prompt = gr.Textbox(placeholder="Your prompt (what you want in place of what is erased)", show_label=False, elem_id="prompt")
+                            btn = gr.Button("Inpaint!", elem_id="run_button")
+                    
+                    with gr.Accordion(label="Advanced Settings", open=False):
+                        with gr.Row(mobile_collapse=False, equal_height=True):
+                            guidance_scale = gr.Number(value=7.5, minimum=1.0, maximum=20.0, step=0.1, label="guidance_scale")
+                            steps = gr.Number(value=20, minimum=10, maximum=30, step=1, label="steps")
+                            strength = gr.Number(value=0.99, minimum=0.01, maximum=0.99, step=0.01, label="strength")
+                            negative_prompt = gr.Textbox(label="negative_prompt", placeholder="Your negative prompt", info="what you don't want to see in the image")
+                        with gr.Row(mobile_collapse=False, equal_height=True):
+                            schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
+                            scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
+                        
+                with gr.Column():
+                    image_out = gr.Image(label="Output", elem_id="output-img", height=400)
+                    mask_out = gr.Image(label="Output Mask", elem_id="output-mask-img", height=400)
+                    with gr.Group(elem_id="share-btn-container", visible=False) as share_btn_container:
+                        community_icon = gr.HTML(community_icon_html)
+                        loading_icon = gr.HTML(loading_icon_html)
+                        share_button = gr.Button("Share to community", elem_id="share-btn",visible=True)
+            
 
-mask_input = gr.inputs.Image(label="Mask", type="sketchpad", invert_colors=True, height=800, width=800, drawing_mode="freedraw", interpretation="mask")
-mask_output = gr.outputs.Image(label="Mask Output", type="numpy", format="png")
+    btn.click(fn=predict, inputs=[image, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, mask_out, share_btn_container], api_name='run')
+    prompt.submit(fn=predict, inputs=[image, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, mask_out, share_btn_container])
+    share_button.click(None, [], [], _js=share_js)
 
-interface = gr.Interface(
-    fn=predict,
-    inputs=mask_input,
-    outputs=mask_output,
-    title="Inpainting Demo",
-    css=css
-)
+ 
+    gr.HTML(
+        """
+            <div class="footer">
+                <p>Model by <a href="https://huggingface.co/diffusers" style="text-decoration: underline;" target="_blank">Diffusers</a> - Gradio Demo by 🤗 Hugging Face
+                </p>
+            </div>
+        """
+    )
 
-interface.launch(debug=True, max_threads=True, share=True, inbrowser=True)
+image_blocks.queue(max_size=25).launch(debug=True, max_threads=True, share=True, inbrowser=True)
