e6b525bd5da11131964c2b1303a53ebbca61a254
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-21 02:18:49 +0300

    Update appl.py

--- appl.py
+++ appl.py
@@ -17,33 +17,58 @@ def read_content(file_path: str) -> str:
 
     return content
 
-
-def predict(dict, invert_mask=False, prompt="", negative_prompt="", guidance_scale=7.5, steps=20, strength=1.0, scheduler="EulerDiscreteScheduler"):
-    if negative_prompt == "":
-        negative_prompt = None
-    scheduler_class_name = scheduler.split("-")[0]
-
-    add_kwargs = {}
-    if len(scheduler.split("-")) > 1:
-        add_kwargs["use_karras"] = True
-    if len(scheduler.split("-")) > 2:
-        add_kwargs["algorithm_type"] = "sde-dpmsolver++"
-
-    scheduler = getattr(diffusers, scheduler_class_name)
-    pipe.scheduler = scheduler.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", subfolder="scheduler", **add_kwargs)
-
-    init_image = dict["image"].convert("RGB").resize((1024, 1024))
-    mask = dict["mask"].convert("RGB").resize((1024, 1024))
+def predict(dict):
+    output_type = dict["output_type"]
+    invert_mask = dict["invert_mask_checkbox"]
     
-    if invert_mask:
-        mask = ImageOps.invert(mask)
-    
-    output = mask
-    return output, gr.update(visible=True)
+    image = dict["image"]
+    init_image = Image.open(image).convert("RGB").resize((1024, 1024))
 
+    if output_type == "Image":
+        output = init_image
+    else:
+        mask = init_image
+        if invert_mask:
+            mask = ImageOps.invert(mask)
+        output = mask
+
+    return output
 
 css = '''
-
+.gradio-container{max-width: 1100px !important}
+#image_upload{min-height:400px}
+#image_upload [data-testid="image"], #image_upload [data-testid="image"] > div{min-height: 400px}
+#mask_radio .gr-form{background:transparent; border: none}
+#word_mask{margin-top: .75em !important}
+#word_mask textarea:disabled{opacity: 0.3}
+.footer {margin-bottom: 45px;margin-top: 35px;text-align: center;border-bottom: 1px solid #e5e5e5}
+.footer>p {font-size: .8rem; display: inline-block; padding: 0 10px;transform: translateY(10px);background: white}
+.dark .footer {border-color: #303030}
+.dark .footer>p {background: #0b0f19}
+.acknowledgments h4{margin: 1.25em 0 .25em 0;font-weight: bold;font-size: 115%}
+#image_upload .touch-none{display: flex}
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+#share-btn-container {padding-left: 0.5rem !important; padding-right: 0.5rem !important; background-color: #000000; justify-content: center; align-items: center; border-radius: 9999px !important; max-width: 13rem; margin-left: auto;}
+div#share-btn-container > div {flex-direction: row;background: black;align-items: center}
+#share-btn-container:hover {background-color: #060606}
+#share-btn {all: initial; color: #ffffff;font-weight: 600; cursor:pointer; font-family: 'IBM Plex Sans', sans-serif; margin-left: 0.5rem !important; padding-top: 0.5rem !important; padding-bottom: 0.5rem !important;right:0;}
+#share-btn * {all: unset}
+#share-btn-container div:nth-child(-n+2){width: auto !important;min-height: 0px !important;}
+#share-btn-container .wrap {display: none !important}
+#share-btn-container.hidden {display: none!important}
+#prompt input{width: calc(100% - 160px);border-top-right-radius: 0px;border-bottom-right-radius: 0px;}
+#run_button{position:absolute;margin-top: 11px;right: 0;margin-right: 0.8em;border-bottom-left-radius: 0px;
+    border-top-left-radius: 0px;}
+#prompt-container{margin-top:-18px;}
+#prompt-container .form{border-top-left-radius: 0;border-top-right-radius: 0}
+#image_upload{border-bottom-left-radius: 0px;border-bottom-right-radius: 0px}
 '''
 
 image_blocks = gr.Blocks(css=css, elem_id="total-container")
@@ -58,14 +83,8 @@ with image_blocks as demo:
                     btn = gr.Button("Inpaint!", elem_id="run_button")
             
             with gr.Row(mobile_collapse=False, equal_height=True):
+                output_type = gr.Radio(["Image", "Mask"], default="Image", label="Output Type", elem_id="output_type")
                 invert_mask_checkbox = gr.Checkbox(label="Invert Mask", initial_value=False, elem_id="invert_mask_checkbox")
-                guidance_scale = gr.Number(value=7.5, minimum=1.0, maximum=20.0, step=0.1, label="guidance_scale")
-                steps = gr.Number(value=20, minimum=10, maximum=30, step=1, label="steps")
-                strength = gr.Number(value=0.99, minimum=0.01, maximum=0.99, step=0.01, label="strength")
-                negative_prompt = gr.Textbox(label="negative_prompt", placeholder="Your negative prompt", info="what you don't want to see in the image")
-            with gr.Row(mobile_collapse=False, equal_height=True):
-                schedulers = ["DEISMultistepScheduler", "HeunDiscreteScheduler", "EulerDiscreteScheduler", "DPMSolverMultistepScheduler", "DPMSolverMultistepScheduler-Karras", "DPMSolverMultistepScheduler-Karras-SDE"]
-                scheduler = gr.Dropdown(label="Schedulers", choices=schedulers, value="EulerDiscreteScheduler")
         
         with gr.Column():
             image_out = gr.Image(label="Output", elem_id="output-img", height=400)
@@ -74,29 +93,30 @@ with image_blocks as demo:
                 loading_icon = gr.HTML(loading_icon_html)
                 share_button = gr.Button("Share to community", elem_id="share-btn", visible=True)
             
-
-    btn.click(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container], api_name='run')
-    invert_mask_checkbox.change(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container])
-    prompt.submit(fn=predict, inputs=[image, invert_mask_checkbox, prompt, negative_prompt, guidance_scale, steps, strength, scheduler], outputs=[image_out, share_btn_container])
+    btn.click(fn=predict, inputs=[image], outputs=[image_out, share_btn_container], api_name='run')
+    output_type.change(fn=predict, inputs=[image], outputs=[image_out, share_btn_container])
+    invert_mask_checkbox.change(fn=predict, inputs=[image], outputs=[image_out, share_btn_container])
+    prompt.submit(fn=predict, inputs=[image], outputs=[image_out, share_btn_container])
     share_button.click(None, [], [], _js=share_js)
 
     gr.Examples(
         examples=[
-            ["./imgs/aaa (8).png"],
-            ["./imgs/download (1).jpeg"],
-            ["./imgs/0_oE0mLhfhtS_3Nfm2.png"],
-            ["./imgs/02_HubertyBlog-1-1024x1024.jpg"],
-            ["./imgs/jdn_jacques_de_nuce-1024x1024.jpg"],
-            ["./imgs/c4ca473acde04280d44128ad8ee09e8a.jpg"],
-            ["./imgs/canam-electric-motorcycles-scaled.jpg"],
-            ["./imgs/e8717ce80b394d1b9a610d04a1decd3a.jpeg"],
-            ["./imgs/Nature___Mountains_Big_Mountain_018453_31.jpg"],
-            ["./imgs/Multible-sharing-room_ccexpress-2-1024x1024.jpeg"],
+            {"image": "./imgs/aaa (8).png"},
+            {"image": "./imgs/download (1).jpeg"},
+            {"image": "./imgs/0_oE0mLhfhtS_3Nfm2.png"},
+            {"image": "./imgs/02_HubertyBlog-1-1024x1024.jpg"},
+            {"image": "./imgs/jdn_jacques_de_nuce-1024x1024.jpg"},
+            {"image": "./imgs/c4ca473acde04280d44128ad8ee09e8a.jpg"},
+            {"image": "./imgs/canam-electric-motorcycles-scaled.jpg"},
+            {"image": "./imgs/e8717ce80b394d1b9a610d04a1decd3a.jpeg"},
+            {"image": "./imgs/Nature___Mountains_Big_Mountain_018453_31.jpg"},
+            {"image": "./imgs/Multible-sharing-room_ccexpress-2-1024x1024.jpeg"},
         ],
         fn=predict,
-        inputs=[image, invert_mask_checkbox],
+        inputs=[image],
         cache_examples=False,
     )
+
     gr.HTML(
         """
         <div class="footer">
@@ -106,4 +126,4 @@ with image_blocks as demo:
         """
     )
 
-image_blocks.queue(max_size=25).launch(debug=True, max_threads=True, share=True, inbrowser=True)
+image_blocks.queue(max_size=25).launch
