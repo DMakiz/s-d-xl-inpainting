decfabc05d399359907c1a07441f380d03e3d2db
DMakiz <142487178+DMakiz@users.noreply.github.com>
2023-09-21 09:49:31 +0300

    Update appl.py

--- appl.py
+++ appl.py
@@ -36,6 +36,7 @@ def predict(dict, invert_mask=False, prompt="", negative_prompt="", guidance_sca
     mask = dict["mask"].convert("RGB").resize((1024, 1024))
     
     if invert_mask:
+        mask = ImageOps.invert(mask)
         output = mask
     else:
         output = pipe(prompt=prompt, negative_prompt=negative_prompt, image=init_image, mask_image=mask, guidance_scale=guidance_scale, num_inference_steps=int(steps), strength=strength).images[0]
